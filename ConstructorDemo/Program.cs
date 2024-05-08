c4ad4ac95e08b97445debb1288694a08f6fabb7f
SE-68\Batch 10 <yubellamargareta@gmail.com>
2024-05-08 08:00:04 +0700

    Constructor demo tambahan

--- ConstructorDemo/Program.cs
+++ ConstructorDemo/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Automotive;
+using Vehicle;
+using Kitchen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Speed speed = new Speed(25.5f);
+        Wheel wheel = new Wheel("Offroad",2023,"Dunlop");
+        Engine engine= new Engine ("Supra",1200,"Toyota");
+        Cake cake= new Cake("RoseBrand");
+        char car= new Car("Black","Toyota",2,engine,wheel,speed);
+        wheel.PrintAttribute();
+        car.PrintAttribute(car.color, car.brand,car.numDoor);
+    }
+}
