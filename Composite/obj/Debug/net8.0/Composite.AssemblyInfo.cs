3d00b4c800e4474255e4dc3646eda8e40f54a6d5
SE-68\Batch 10 <yubellamargareta@gmail.com>
2024-06-11 15:01:38 +0700

    review

--- Composite/obj/Debug/net8.0/Composite.AssemblyInfo.cs
+++ Composite/obj/Debug/net8.0/Composite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Composite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de18d8986c33eb91c2b1a6b3ed1189f531017be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9022fa3411229482f5592977d74b594bcd044dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Composite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Composite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
