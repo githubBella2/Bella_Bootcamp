ee084dab93f06b95765da11eea5ffa576d3fabe9
githubBella2 <yubellamargareta@gmail.com>
2024-06-14 14:37:50 +0700

    web api dto data transfer objct

--- MyWebAPI/Controllers/CategoryController.cs
+++ MyWebAPI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using MyWebAPI.DTOS;
 using MyWebAPI.Models;
 
 namespace MyWebAPI.Controllers;
@@ -24,13 +25,53 @@ public class CategoryController : ControllerBase
         return Ok(_db.Categories.ToList());
     }
 
+    // [HttpPost]
+    // public IActionResult CreateCategory(Category category) //menambahkan kategori baru
+    // {
+    //     _db.Categories.Add(category);
+    //     _db.SaveChanges();
+    //     return Ok();
+    // }
+
+    // DTO
     [HttpPost]
-    public IActionResult PostCategory(Category category) //menambahkan kategori baru
+    public IActionResult CreateCategoryDTO(CategoryDTO category) //menambahkan kategori baru
     {
-        _db.Categories.Add(category);
+        Category categoryNew = new()
+        {
+            CategoryName = category.CategoryName,
+            Description=category.Description
+        };
+        _db.Categories.Add(categoryNew);
         _db.SaveChanges();
         return Ok();
+    }
 
+    [HttpPut]
+    public IActionResult UpdateCategory(int id, Category category)
+    {
+        Category cat = _db.Categories.Find(id);
+        if (cat == null)
+        {
+            return NotFound($"ID {id} not found.");
+        }
+        cat.CategoryName = category.CategoryName;
+        cat.Description = category.Description;
+        _db.SaveChanges();
+        return Ok(cat);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteCategory(int id)
+    {
+        Category cat = _db.Categories.Find(id);
+        if (cat == null)
+        {
+            return NotFound($"ID {id} not found.");
+        }
+        _db.Remove(cat);
+        _db.SaveChanges();
+        return Ok(cat);
     }
 }
 
---
+++ MyWebAPI/DTOS/CategoryDTO.cs
@@ -0,0 +1,7 @@
+namespace MyWebAPI.DTOS;
+
+public class CategoryDTO
+{
+     public string CategoryName { get; set; }
+    public string Description { get; set; }
+}
